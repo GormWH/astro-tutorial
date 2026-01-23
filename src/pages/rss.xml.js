91a271ff769a421a81fc3014111cfd1cc9843439
GormWH <sp4kid@gmail.com>
2026-01-24 03:11:42 +0900

    chapter6: The END

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+const baseUrl = import.meta.env.BASE_URL;
 
 export async function GET(context) {
+  const posts = await getCollection('blog');
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `${baseUrl}/posts/${post.id}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
